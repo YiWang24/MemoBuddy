95c3f2345ad45daf22e5da138d5fc82d6ce7d400
komalpreet05 <komalbhatia2305@gmail.com>
2024-11-18 21:36:43 -0500

    navbar functionality worked

--- frontend/src/components/navigation/Navbar.jsx
+++ frontend/src/components/navigation/Navbar.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+// import React from "react";
 import Button from "./Button";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchLogout } from "@/lib/features/auth/authSlice";
+import { useRouter } from "next/navigation";
+import { message } from "antd";
 
 const Navbar = () => {
+
+    const [current, setCurrent] = useState("Home");
+    const [authStatus, setAuthStatus] = useState(false);
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+    const dispatch = useDispatch();
+    const router = useRouter();
+    // const handleOnClick = (e) => {
+    //     console.log("click ", e);
+    //     setCurrent(e.key);
+    // };
+    const handleLogout = async () => {
+        await dispatch(fetchLogout());
+        message.success("Logout successful");
+        router.push("/login");
+    };
+
+    useEffect(() => {
+        setAuthStatus(isAuthenticated);
+    });
+
     return (
-        <nav className="bg-white text-black px-6 py-4 border-b-2 border-black">
-            <div className="container mx-auto flex items-center justify-between">
+        <div className="bg-white text-black px-6 py-4 border-b-2 border-black">
+            <nav className="container mx-auto flex items-center justify-between">
                 {/* Logo */}
-                <div className="text-2xl font-bold">
+                <div>
                     <a href="/"><img src={"/logo.svg"} alt="memmobuddy" height={150} width={150}></img></a>
                 </div>
 
@@ -17,12 +42,12 @@ const Navbar = () => {
                         </a>
                     </li>
                     <li>
-                        <a href="/about" className="hover:text-gray-300">
+                        <a href="/about" className="hover:font-semibold">
                             About
                         </a>
                     </li>
                     <li>
-                        <a href="#services" className="hover:text-gray-300">
+                        <a href="#services" className="hover:font-semibold">
                             Services
                         </a>
                     </li>
@@ -30,20 +55,33 @@ const Navbar = () => {
 
                 {/* Buttons */}
                 <div className="space-x-4">
-                    <a href="/login">
-                        <Button>
-                            Login
-                        </Button>
-                    </a>
-                    <a href="/register">
-                        <Button>
-                            Signup
-                        </Button>
-                    </a>
+                    {
+                        authStatus ?
+                            (
+                                <Button onClick={handleLogout}>
+                                    Log Out
+                                </Button>
+                            ) :
+                            (
+                                <div className="space-x-4">
+                                    <a href="/login">
+                                        <Button>
+                                            Login
+                                        </Button>
+                                    </a>
+                                    <a href="/register">
+                                        <Button>
+                                            Signup
+                                        </Button>
+                                    </a>
+                                </div>
+                            )
+                    }
+
 
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     );
 };
 
