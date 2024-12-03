8833e911f74d8b65820135b80e99592ff71bb671
Fares Islam <islamfares23.official@gmail.com>
2024-12-02 23:38:58 -0500

    I have made changes in the src:app:about (page.jsx, style.css) and contact: page.jsx)

--- frontend/src/app/about/page.jsx
+++ frontend/src/app/about/page.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./style.css";
+
+// Import components for the "About" page
 import MeetOurTeam from "@/components/about/MeetOurTeam";
 import AboutBanner from "@/components/about/AboutBanner";
 import FeedbackForm from "@/components/about/FeedbackForm";
 
+// Define and export the "About" page component
 export default function page() {
   return (
     <main className="about-page">
-
+      {/* Welcome Section */}
       <section className="welcome-section">
         <h2 className="welcome-title">Welcome to Our Secure Diary</h2>
+        {/* Description of the diary's purpose and features */}
         <p className="welcome-description">
           Our password-protected diary is designed to offer a safe and private
           space for your thoughts, memories, and daily reflections. With
@@ -23,14 +27,13 @@ export default function page() {
         </p>
       </section>
 
+      {/* The react components imported above are being used in the section below*/}
       <AboutBanner></AboutBanner>
       <MeetOurTeam></MeetOurTeam>
-      <h2 className='text-center p-6 text-3xl mt-6 font-semibold'>
+      <h2 className="text-center p-6 text-3xl mt-6 font-semibold">
         Can you help us improve?
       </h2>
       <FeedbackForm></FeedbackForm>
     </main>
   );
 }
-
-
--- frontend/src/app/contact/page.jsx
+++ frontend/src/app/contact/page.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+// Import the Contact and FAQ components
 import Contact from "@/components/Contact/Contact";
 import FAQ from "@/components/Contact/FAQ";
 
+// Define and export the Wrapper component
 export default function Wrapper() {
   return (
     <>
+      {/* Header section with a background image */}
       <div
         className="relative h-[45vh] md:h-[60vh] bg-cover bg-center"
         style={{
@@ -12,9 +15,12 @@ export default function Wrapper() {
         }}
       >
       </div>
+
+      {/* The Contact section imported above is being used here */}
       <Contact></Contact>
+
+      {/* The FAQ section imported above is being used here */}
       <FAQ></FAQ>
     </>
-
   );
 }
